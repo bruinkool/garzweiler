46ee1377f6771c6ab0a7f5ae7e0da9a4e2e60969
Mike de Heij <mike@deheij.net>
2021-01-12 17:42:50 +0100

    Add user ID and roles to JWT token claims

--- src/main/java/land/bruinkool/garzweiler/security/jwt/JwtUtils.java
+++ src/main/java/land/bruinkool/garzweiler/security/jwt/JwtUtils.java
@@ -1,15 +1,14 @@
 package land.bruinkool.garzweiler.security.jwt;
 
-import land.bruinkool.garzweiler.security.userdetails.BruinkoolUserDetails;
 import io.jsonwebtoken.*;
+import land.bruinkool.garzweiler.security.userdetails.BruinkoolUserDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.Map;
 
@@ -26,15 +25,23 @@ public class JwtUtils {
 	@Value("${jwtExpirationMs}")
 	private int jwtExpirationMs;
 
+	@Value("${jwtKeyId}")
+	private int jwtKeyId;
+
 	public String generateJwtToken(Authentication authentication) {
 		BruinkoolUserDetails userPrincipal = (BruinkoolUserDetails) authentication.getPrincipal();
 
+		Map<String, Object> claims = Map.of(
+				"kid", jwtKeyId,
+				"uid", userPrincipal.getId(),
+				"roles", userPrincipal.getAuthorities().stream().map(GrantedAuthority::getAuthority).toArray()
+		);
 		return Jwts.builder()
 				.setSubject((userPrincipal.getUsername()))
 				.setIssuedAt(new Date())
 				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
 				.setAudience(jwtAudience)
-				.addClaims(Map.of("kid", 1, "id", userPrincipal.getId()))
+				.addClaims(claims)
 				.signWith(SignatureAlgorithm.HS512, jwtSecret)
 				.compact();
 	}
